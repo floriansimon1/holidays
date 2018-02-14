eadf24d72c485e397db0eed0abb3f645777ddde4
Florian Simon <floriansimon9@gmail.com>
2018-02-15 00:55:24 +0100

    In production, run on port 80

--- source/configuration.js
+++ source/configuration.js
@@ -2,7 +2,7 @@ const environment = process.env.NODE_ENV || 'development';
 
 const configurations = {
   development: { port: 16000 },
-  production:  { port: 16001 },
+  production:  { port: 80    },
   test:        { port: 16002 }
 };
 
